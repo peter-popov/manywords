324443d8c34cb46362cf644097226d50d2453770
Peter Popov <petro.popov@gmail.com>
2012-02-27 00:16:16 +0100

    word editor improved

--- ManyWords/MainPage.xaml.cs
+++ ManyWords/MainPage.xaml.cs
@@ -21,11 +21,6 @@ namespace ManyWords
     public partial class MainPage : PhoneApplicationPage
     {
 
-        ITranslator default_translator = TranslatorFactory.CreateInstance();
-
-        Language from = new Language { Code = "de", Name = "German" };
-        Language to = new Language { Code = "ru", Name = "Russian" };
-        
         // Constructor
         public MainPage()
         {
@@ -34,84 +29,22 @@ namespace ManyWords
             // Set the data context of the listbox control to the sample data           
             DataContext = App.TrainingsViewModel;
 
-            this.Loaded += new RoutedEventHandler(MainPage_Loaded);
-        }
-
-        // Load data for the ViewModel Items
-        private void MainPage_Loaded(object sender, RoutedEventArgs e)
-        {
-
-            FrameworkDispatcher.Update();
-
-            default_translator.TranslateComplete += TranslateCompleted;
-            default_translator.SpeachReady += translator_SpeakCompleted;
-        }        
-
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-
-            default_translator.StartTranslate(fromTxt.Text, from, to); 
-        }
-
-        private void btnSpeak_Click(object sender, RoutedEventArgs e)
-        {
-            default_translator.StartSpeach(fromTxt.Text, from);
-        }
-        
-
-
-        void TranslateCompleted(object sender, TranslatedEventArgs<List<string>> e)
-        {
-            toTxt.Text = "";
-            foreach( string s in e.Result)
-                toTxt.Text += s + "\n";
         }
 
 
-        bool is_save_mode = false;
-
-        void translator_SpeakCompleted(object sender, TranslatedEventArgs<Stream> e)
+        private void PhoneApplicationPage_Loaded(object sender, RoutedEventArgs e)
         {
-            if (!is_save_mode)
-            {
-                //SoundEffect se = SoundEffect.FromStream(e.Result);
-                //se.Play();
-            }
-            else
-            {
-                using (WordStorage.Storage storage = new WordStorage.Storage())
-                {
-                    WordStorage.Word new_word = new WordStorage.Word { Spelling = fromTxt.Text };
-                    
 
-                    storage.StoreWord(fromTxt.Text, toTxt.Text.Split(new char[]{'\n'}), e.Result);
-                }
-            }
-
-            is_save_mode = false;
         }
 
-        private void btnWords_Click(object sender, RoutedEventArgs e)
+        private void ApplicationBarButtonAddWord_Click(object sender, EventArgs e)
         {
-            Uri uri = new Uri("/Views/WordsView.xaml", UriKind.Relative);
-            System.Diagnostics.Debug.WriteLine(uri.ToString());
-            NavigationService.Navigate(uri);
+            NavigationService.Navigate(new Uri("/Views/WordEditor.xaml", UriKind.Relative));   
         }
 
-       
-        private void btnLearn_Click(object sender, RoutedEventArgs e)
+        private void ApplicationBarButtonWordsList_Click(object sender, EventArgs e)
         {
-            is_save_mode = true;
-            default_translator.StartSpeach(toTxt.Text, to);
-        }
-
-        private void button1_Click(object sender, RoutedEventArgs e)
-        {
-            Uri uri = new Uri("/Views/WordEditor.xaml", UriKind.Relative);
-            System.Diagnostics.Debug.WriteLine(uri.ToString());
-            NavigationService.Navigate(uri);
+            NavigationService.Navigate(new Uri("/Views/WordsView.xaml", UriKind.Relative));
         }
     }
 }
--- ManyWords/Views/WordEditor.xaml.cs
+++ ManyWords/Views/WordEditor.xaml.cs
@@ -10,14 +10,81 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using ManyWords.Translator;
+using System.IO;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 
 namespace ManyWords.Views
 {
     public partial class WordEditor : PhoneApplicationPage
     {
+        ITranslator default_translator = TranslatorFactory.CreateInstance();
+
+        Language from = new Language { Code = "de", Name = "German" };
+        Language to = new Language { Code = "ru", Name = "Russian" };
+
         public WordEditor()
         {
             InitializeComponent();
         }
+
+
+        private void WordEditor_Loaded(object sender, RoutedEventArgs e)
+        {
+            FrameworkDispatcher.Update();
+
+            default_translator.TranslateComplete += translator_TranslateCompleted;
+            default_translator.SpeachReady += translator_SpeakCompleted;
+
+            txtWord.TextInput += txtWord_TextChanged;
+        }
+
+        private void txtWord_TextChanged(object sender, RoutedEventArgs e)
+        {
+            default_translator.StartTranslate(txtWord.Text, from, to);     
+        }
+
+        private void btnTranslate_Click(object sender, RoutedEventArgs e)
+        {
+            default_translator.StartTranslate(txtWord.Text, from, to); 
+        }
+
+        void translator_TranslateCompleted(object sender, TranslatedEventArgs<List<string>> e)
+        {
+            if (e.IsOk == false)
+            {
+                //Do something
+                return;
+            }
+            txtTranslations.Text = "";
+            e.Result.ForEach( s => txtTranslations.Text += s + "\n" );            
+        }
+        
+
+        private void btnSpeak_Click(object sender, RoutedEventArgs e)
+        {
+            default_translator.StartSpeach(txtWord.Text, from);
+        }
+
+        void translator_SpeakCompleted(object sender, TranslatedEventArgs<Stream> e)
+        {
+            if (e.IsOk == false)
+            {
+                //Do something
+                return;
+            }
+            SoundEffect se = SoundEffect.FromStream(e.Result);
+            se.Play();            
+        }
+
+
+        private void btnDone_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+
     }
 }
