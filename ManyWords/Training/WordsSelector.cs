a7278a6c3c85b080bd06fad6be048d75180f219e
Peter Popov <petro.popov@gmail.com>
2012-06-12 22:08:51 +0200

    minor fix

--- ManyWords/Training/WordsSelector.cs
+++ ManyWords/Training/WordsSelector.cs
@@ -67,8 +67,8 @@ namespace ManyWords.WordStorage
 
             var res = from Translation t in storage.wordsDB.Translations
                       where t.Language == languageMother && 
-                            t.Spelling != main.Spelling && 
-                            t.ID != w.WordID && (((t.ID + offset) ^ mask) % 10 > 5)                                   
+                            t.Spelling != main.Spelling &&
+                            t.wordID != w.WordID && (((t.ID + offset) ^ mask) % 10 > 5)                                   
                       select t;
 
             return res.Take(count);
